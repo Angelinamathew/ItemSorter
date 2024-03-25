b7f98145827d10e893761ab24a0f3de186c14d36
Angelinamathew <angelinamathew707@gmail.com>
2024-03-24 23:57:00 -0400

    Updated

--- src/main/java/rocks/zipcode/io/comparators/IdComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/IdComparator.java
@@ -1,7 +1,17 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class IdComparator {
+public class IdComparator implements Comparable<Item> {
+    public int compareTo(Item o1,Item o2) {
+        return (int) (o1.getId() - o2.getId()) ;
+    }
+
+    @Override
+    public int compareTo(Item o) {
+        return 0;
+    }
 }
